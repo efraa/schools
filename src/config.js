69e6f3f2f5991fa1e44e40f4b293f15dc5e11e7c
Efra Peralta <efra23peralta@gmail.com>
2020-03-12 00:29:24 -0400

    feat(config): add form validators

--- src/config.js
+++ src/config.js
@@ -1,6 +1,19 @@
 export const config = {
   jwt: {
     EXPIRE: process.env.REACT_APP_TOKEN_EXPIRE,
-    SECRET: process.env.REACT_APP_SECRET,
+    SECRET: process.env.REACT_APP_TOKEN_SECRET,
   },
+}
+
+export const validations = {
+  messages: {
+    required: ':attribute is required.',
+    email: 'Your :attribute address must be valid.',
+    alpha_num_dash:
+      'The :attribute can only contain letters, numbers and hyphens.',
+    min: ':attribute must be at least 6 characters.',
+    alpha_space: ':attribute can only contain letters and spaces.',
+    alpha_num: ':attribute can only contain letters and numbers.',
+  },
+  locale: 'en',
 }
